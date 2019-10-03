7cdbb2f3834f67b687a9739fcc99b9d6417a3e5d
Thomas Grainger <tagrain@gmail.com>
2019-10-03 20:19:26 +0100

    format package for setup-py-upgrade

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-from os import path
-import io
+from setuptools import setup, find_packages
 
-from setuptools import setup
-
-this_directory = path.abspath(path.dirname(__file__))
-with io.open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open("README.md") as f:
     long_description = f.read()
 
 setup(
     name="format_cef",
-    packages=["format_cef"],
     description=(
         "A small helper for formatting ArcSight Common Event Format (CEF) "
         "compliant messages"
@@ -20,4 +15,6 @@ setup(
     install_requires=[],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=find_packages('src'),
+    package_dir={'':'src'},
 )
